115ae96824b33b9e61e78eb845b6cbd891180e02
Cavan Chung <cavan.chung23@ycdsbk12.ca>
2020-12-02 22:33:19 +0000

    Practice 3

---
+++ cake_jog.py
@@ -0,0 +1,5 @@
+cake = float(input("How many pieces of cake have you eaten:"))
+
+cake_jog= ((225*cake)/100)
+
+print ("You will need to run this much km:" + str(cake_jog))
--- oz_to_ml.py
+++
@@ -1,4 +0,0 @@
-oz= float(input("Enter the amount of ounces:"))
-people = float(input("Enter the number of people to serve:"))
-oz_to_ml= ((oz*29.57)*people)/4
-print("You will need this much ml:" + str(oz_to_ml))
